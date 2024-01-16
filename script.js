060a28b9fede91cbbefa12c07ff608a0cfdc1a33
wyclefjean <155137336+wyclefjean@users.noreply.github.com>
2024-01-16 12:46:16 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+if (isOpen) {
+    sideMenu.style.right = '-250px';
+    document.body.style.backgroundColor = '#FFF';
+} else {
+    sideMenu.style.right = '0px';
+    document.body.style.backgroundColor = 'rgba(0,0,0,0.5)';
+}
